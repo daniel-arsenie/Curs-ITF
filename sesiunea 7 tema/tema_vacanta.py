f9663e906935e03fe7f8ac6ce376eefd608b32ac
daniel-arsenie <daniel.arsenie@gmail.com>
2023-01-06 17:55:48 +0200

    tema 7+vacanta

--- sesiunea 7 tema/tema_vacanta.py
+++ sesiunea 7 tema/tema_vacanta.py
@@ -1,3 +1,6 @@
+from abc import abstractmethod, ABC
+
+
 # De raspuns la intrebarile:
 # 1. Ce inseamna if __name__ == "__main__" scris intr - un fisier python?
 # Raspuns: pastreaza o bucata de cod pentru a fi rulat
@@ -63,3 +66,158 @@ s.add(p)
 # s.add(p2)
 
 print(s)
+
+
+# 2. Rezolvă exercițiul după ce ai urcat proiectul (tot ce am facut până acum
+# împreună).
+# ABSTRACTION
+# Clasa abstractă FormaGeometrica
+# Conține un field PI=3.14
+# Conține o metodă abstractă aria (opțional)
+# Conține o metodă a clasei descrie() - aceasta printează pe ecran ‘Cel mai
+# probabil am colturi’
+
+class FormaGeometrica(ABC):
+    pi = 3.14
+
+    @abstractmethod
+    def aria(self):
+        pass
+
+    def descrie(self):
+        print('Cel mai probabil am colturi.')
+
+
+# INHERITANCE
+# Clasa Pătrat - moștenește FormaGeometrica
+# constructor pentru latură
+
+class Patrat(FormaGeometrica):
+    def __init__(self, latura):
+        self.__latura = latura
+
+    @property
+    def latura(self):
+        print('Setting as property:')
+        return self.__latura
+
+    @latura.getter
+    def latura(self):
+        print('Getting value:')
+        return self.__latura
+
+    @latura.setter
+    def latura(self, noua_latura):
+        print('Settin new value to "latura":')
+        self.__latura = noua_latura
+
+    @latura.deleter
+    def latura(self):
+        print('Deleting value')
+        self.__latura = None
+
+    def aria(self):
+        return f'Aria patratului este: {self.__latura ** 2}'
+
+
+# ENCAPSULATION
+# latura este proprietate privată
+# Implementează getter, setter, deleter pentru latură
+# Implementează metoda cerută de interfață (opțional, doar dacă ai ales să
+# implementezi metoda abstractă aria)
+
+# Clasa Cerc - moștenește FormaGeometrica
+# constructor pentru rază
+# raza este proprietate privată
+# Implementează getter, setter, deleter pentru rază
+# Implementează metoda cerută de interfață - în calcul folosește field PI
+# mostenit din clasa părinte (opțional, doar dacă ai ales să implementezi metoda
+# abstractă aria)
+
+class Cerc(FormaGeometrica):
+    def __init__(self, raza):
+        self.__raza = raza
+
+    @property
+    def raza(self):
+        print('Setting as property:')
+        return self.__raza
+
+    @raza.getter
+    def raza(self):
+        print('Getting value:')
+        return self.__raza
+
+    @raza.setter
+    def raza(self, noua_raza):
+        print('Settin new value to "raza":')
+        self.__raza = noua_raza
+
+    @raza.deleter
+    def raza(self):
+        print('Deleting value')
+        self.__raza = None
+
+    def aria(self):
+        print(f'Aria este: {self.pi * (self.__raza ** 2)}')
+
+    def descrie(self):
+        print('Eu nu am colturi.')
+
+
+# POLYMORPHISM
+# Definește o nouă metodă descrie - printează ‘Eu nu am colturi’
+# Creează un obiect de tip Pătrat și joacă-te cu metodele lui
+# Creează un obiect de tip Cerc și joacă-te cu metodele lui
+# 3. Actualizează proiectul pe github facand un push la noul cod
+# În Folderul de teme ajunge să pui doar linkul cu proiectul tău public
+# Curs git/github
+# https://bit.ly/3yfFvqL - VIDEO 4
+
+p1 = Patrat(25)
+p2 = Patrat(10)
+
+print(p1.latura)
+print(p2.latura)
+
+p1.latura = 16
+print(p1.latura)
+p2.latura = 8
+print(p2.latura)
+
+# del p1.latura
+# print(p1.latura)
+# del p2.latura
+# print(p1.latura)
+
+print(p1.aria())
+print(p2.aria())
+
+p1.descrie()
+p2.descrie()
+
+print(50 * "#")
+
+########################################
+
+c1 = Cerc(10)
+c2 = Cerc(15)
+
+print(c1.raza)
+print(c2.raza)
+
+c1.raza = 12
+print(c1.raza)
+c2.raza = 21
+print(c2.raza)
+
+# del c1.raza
+# print(c1.raza)
+# del c2.raza
+# print(c2.raza)
+
+c1.aria()
+c2.aria()
+
+c1.descrie()
+c2.descrie()
