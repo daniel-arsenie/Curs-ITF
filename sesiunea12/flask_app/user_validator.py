61d8c54d3d22fe953179a905923529e2e9c7a92f
daniel-arsenie <daniel.arsenie@gmail.com>
2023-02-14 09:31:02 +0200

    added PATCH

---
+++ sesiunea12/flask_app/user_validator.py
@@ -0,0 +1,9 @@
+from exceptions import InvalidUserException
+
+
+class UserValidator:
+    def validate_user(self, user):
+        if "name" not in user:
+            raise InvalidUserException("No name provided")
+        if "age" not in user:
+            raise InvalidUserException("No age provided")
