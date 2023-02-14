72555a778e14cb67f9b23f982b74eb7ae90b3790
daniel-arsenie <daniel.arsenie@gmail.com>
2023-02-14 09:32:35 +0200

    added homework exercise 2

---
+++ sesiunea12/flask_homework/user_validator.py
@@ -0,0 +1,11 @@
+from exceptions import InvalidUserException
+
+
+class UserValidator:
+    def validate_user(self, user):
+        if "name" not in user:
+            raise InvalidUserException("No name provided")
+        if "product" not in user:
+            raise InvalidUserException("No product provided")
+        if "car" not in user:
+            raise InvalidUserException("No car provided")
