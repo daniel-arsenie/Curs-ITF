3532ccbd04bc6c10b8a296f8d6769e2b815f260a
Daniel <daniel.arsenie@gmail.com>
2023-03-31 16:20:14 +0300

    added Homepage, sum of amount and general tweaks

--- sesiunea14/finance_manager/spendings/urls.py
+++ sesiunea14/finance_manager/spendings/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = 'spendings'
 
 urlpatterns = [
-    path('', views.spendings, name='all_spendings'),
-    path('<int:spending_id>/', views.spending_detail, name='spending_details'),
-    path('spending/', views.spending_add, name='add')
+    path("", views.index, name="index"),
+    path("all_spendings/", views.SpendingsView.as_view(), name="all_spendings"),
+    path("<int:pk>/", views.SpendingDetailView.as_view(), name="spending_details"),
+    path("spending/", views.SpendingCreateView.as_view(), name="add"),
 ]
--- sesiunea14/finance_manager/spendings/views.py
+++ sesiunea14/finance_manager/spendings/views.py
@@ -1,6 +1,9 @@
+from django.db.models import Sum
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from django.urls import reverse
+from django.template import loader
+from django.urls import reverse, reverse_lazy
+from django.views import generic
 
 from .models import Spending
 
@@ -8,17 +11,24 @@ from .models import Spending
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello world, Here is Spendings index.')
+    spendings_filter = Spending.objects.values('location').distinct()
+    spendings_total = Spending.objects.aggregate(Sum('amount'))
+    budget_left = 10000 - spendings_total['amount__sum']
+
+    return render(request, 'index.html', {'spendingsfilter': spendings_filter,
+                                          'spendingstotal': spendings_total,
+                                          'budgetleft': budget_left})
 
 
-def spendings(request):
-    spendings_list = Spending.objects.all
-    return render(request, 'all_spendings.html', {'spendings': spendings_list})
+# def spendings(request):
+#     spendings_list = Spending.objects.all
+#     return render(request, 'all_spendings.html', {'spendings': spendings_list})
 
 
 def spending_detail(request, spending_id):
     spending = get_object_or_404(Spending, pk=spending_id)
     return render(request, 'spending_details.html', {'spending': spending})
+
 
 def spending_add(request):
     spending_data = request.POST.dict()
@@ -33,3 +43,30 @@ def spending_add(request):
     except KeyError:
         return render(request, 'error.html', {'error_message': 'Error! Invalid spending details!'})
     return HttpResponseRedirect(reverse('spendings:all_spendings'))
+
+
+# VERSION 02 - IMPROVED
+class SpendingsView(generic.ListView):
+    template_name = "all_spendings.html"
+    context_object_name = "spendings"
+
+    def get_queryset(self):
+        return Spending.objects.all()
+
+
+class SpendingDetailView(generic.DetailView):
+    model = Spending
+    template_name = "spending_details.html"
+
+
+class SpendingCreateView(generic.CreateView):
+    model = Spending
+    fields = ["location", "amount", "description", "paid_with"]
+    success_url = reverse_lazy("spendings:all_spendings")
+
+
+def spendings(request):
+    spendings_filter = Spending.objects.values('location')
+    spendings_total = Spending.objects.aggregate(Sum('amount'))
+    return render(request, 'all_spendings.html', {'spendingsfilter': spendings_filter,
+                                                  'spendingstotal': spendings_total})
